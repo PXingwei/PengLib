5fc7a726031d2656b8bb00707d37080e060ae484
PXingwei <32804492+PXingwei@users.noreply.github.com>
2017-10-14 16:24:03 -0700

    Object.cpp

---
+++ Object.cpp
@@ -0,0 +1,38 @@
+#include <cstdlib>
+#include "Object.h"
+
+namespace PengLib
+{
+	/**Overload the new operator */
+    void* Object::operator new(unsigned int size) throw()
+	{
+		return malloc(size);
+	}
+	/**Overload the new[] operator */
+    void* Object::operator new[](unsigned int size) throw()
+	{
+		return  malloc(size);
+	}
+	/**Overload the delete operator */
+    void Object::operator delete (void* pn)
+	{
+		free(pn);
+	}
+	/**Overload the delete[] operator */
+	void Object::operator delete[] (void* pn)
+	{
+		free(pn);
+	}
+	/**Overload the == operator */
+    bool Object::operator == (const Object& e)
+    {
+        return (this == &e);
+    }
+    /**Overload the != operator */
+    bool Object::operator != (const Object& e)
+    {
+        return (this != &e);
+    }
+
+	Object::~Object() {}
+}
